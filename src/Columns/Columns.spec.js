3e0cd57537ed4ab3b51c71893cb64d155e0c8030
davidcraig <davidcraig1986@gmail.com>
2020-03-04 12:56:16 +0000

    Fix spec for column components.

---
+++ src/Columns/Columns.spec.js
@@ -0,0 +1,18 @@
+// Link.react.test.js
+import React from 'react'
+import { Column, Columns } from './Columns'
+import renderer from 'react-test-renderer'
+import { cleanup } from '@testing-library/react'
+
+afterEach(cleanup)
+
+test('It renders as expected', () => {
+  const component = renderer.create(
+    <Columns>
+      <Column>I am col 1</Column>
+      <Column>I am col 2</Column>
+      <Column>I am col 3</Column>
+    </Columns>
+  )
+  expect(component.toJSON()).toMatchSnapshot()
+})
