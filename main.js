a35e9105e7d3c96d9abecce7f103c408e1958429
David Craig <davidcraig1986@gmail.com>
2020-03-12 22:57:54 +0000

    Transpile main.js to npm

--- main.js
+++ main.js
@@ -1,5 +1,87 @@
-export { Card } from './dist/Card/Card'
-export { Breadcrumb, Breadcrumbs } from './dist/Breadcrumbs/Breadcrumbs'
-export { Column, Columns } from './dist/Columns/Columns'
-export { Navbar, Brand, Start, End, Item, Dropdown } from './dist/Navbar/Navbar'
-export { TabbedContent } from './dist/TabbedContent/TabbedContent'
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Card", {
+  enumerable: true,
+  get: function get() {
+    return _Card.Card;
+  }
+});
+Object.defineProperty(exports, "Breadcrumb", {
+  enumerable: true,
+  get: function get() {
+    return _Breadcrumbs.Breadcrumb;
+  }
+});
+Object.defineProperty(exports, "Breadcrumbs", {
+  enumerable: true,
+  get: function get() {
+    return _Breadcrumbs.Breadcrumbs;
+  }
+});
+Object.defineProperty(exports, "Column", {
+  enumerable: true,
+  get: function get() {
+    return _Columns.Column;
+  }
+});
+Object.defineProperty(exports, "Columns", {
+  enumerable: true,
+  get: function get() {
+    return _Columns.Columns;
+  }
+});
+Object.defineProperty(exports, "Navbar", {
+  enumerable: true,
+  get: function get() {
+    return _Navbar.Navbar;
+  }
+});
+Object.defineProperty(exports, "Brand", {
+  enumerable: true,
+  get: function get() {
+    return _Navbar.Brand;
+  }
+});
+Object.defineProperty(exports, "Start", {
+  enumerable: true,
+  get: function get() {
+    return _Navbar.Start;
+  }
+});
+Object.defineProperty(exports, "End", {
+  enumerable: true,
+  get: function get() {
+    return _Navbar.End;
+  }
+});
+Object.defineProperty(exports, "Item", {
+  enumerable: true,
+  get: function get() {
+    return _Navbar.Item;
+  }
+});
+Object.defineProperty(exports, "Dropdown", {
+  enumerable: true,
+  get: function get() {
+    return _Navbar.Dropdown;
+  }
+});
+Object.defineProperty(exports, "TabbedContent", {
+  enumerable: true,
+  get: function get() {
+    return _TabbedContent.TabbedContent;
+  }
+});
+
+var _Card = require("./dist/Card/Card");
+
+var _Breadcrumbs = require("./dist/Breadcrumbs/Breadcrumbs");
+
+var _Columns = require("./dist/Columns/Columns");
+
+var _Navbar = require("./dist/Navbar/Navbar");
+
+var _TabbedContent = require("./dist/TabbedContent/TabbedContent");
