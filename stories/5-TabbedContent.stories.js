8448d3584cb551971d5ed398b1d992c04a0a4fff
David Craig <davidcraig1986@gmail.com>
2020-03-17 10:23:06 +0000

    Update tabbedcontent storybook.

---
+++ stories/5-TabbedContent.stories.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import 'bulma/css/bulma.css'
+import TabbedContent from '../src/TabbedContent/TabbedContent'
+
+export default {
+  title: 'TabbedContent',
+  component: TabbedContent
+}
+
+export const TabbedContentStory = () => (
+  <>
+    <div class="container">
+      <div class="content">
+        <h1>TabbedContent</h1>
+
+        <p>Import:</p>
+        <p><code>{`import { TabbedContent } from '@davidcraig/react-bulma'`}</code></p>
+
+        <h2>Standard Example</h2>
+        
+        <pre><code>{`<TabbedContent content={{
+  tab1: { title: 'Tab 1', content: 'Hello from tab 1' },
+  tab2: { title: 'Tab 2', content: 'Hello from tab 2' }
+}}></TabbedContent>`
+}</code></pre>
+      </div>
+
+      <TabbedContent content={{ tab1: { title: 'Tab 1', content: 'Hello from tab 1' }, tab2: { title: 'Tab 2', content: 'Hello from tab 2' } }}></TabbedContent>
+    </div>
+  </>
+)
+
+TabbedContentStory.story = {
+  name: 'Tabbed Content'
+}
